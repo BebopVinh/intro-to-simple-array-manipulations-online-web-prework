495a071b83aafb1db050112527bada8fe95e7e96
Vinh Do <vinhdo07@gmail.com>
2019-01-16 04:01:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,15 @@
 def using_push(array, u_string)
   array.push(u_string)
 end
+
+def using_unshift(array, u_string)
+  array.unshift(u_string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
